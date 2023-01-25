b2af4e41db40db669855eff5d2c8776587ecf2a0
unknown <krisykord@gmail.com>
2023-01-25 08:27:12 -0800

    axios config

---
+++ src/services/axios/index.js
@@ -0,0 +1,26 @@
+const { default: axios } = require("axios");
+
+const instance = axios.create({
+  baseURL: "https://imdb-api.com/en/API/",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+instance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+const successHandler = (response) => response;
+
+const errorHandler = (error) => Promise.reject(error);
+
+instance.interceptors.response.use(successHandler, errorHandler);
+
+export default instance;
