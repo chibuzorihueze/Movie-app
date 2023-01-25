488bc26b012e92621d68c0a63356fc520f8d62ca
unknown <krisykord@gmail.com>
2023-01-25 06:29:12 -0800

    setup and config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Navbarcomp } from './components/Navbarcomp'
 
+
 export default function App() {
   return (
     <div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import Navbarcomp from "./Navbarcomp";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Navbarcomp />
+      <div>
+        {}
+      </div>
+    </>
+  );
+};
