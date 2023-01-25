8b1c95c9eb96d9c089999b6f507cdb653eff6e96
unknown <krisykord@gmail.com>
2023-01-25 09:15:31 -0800

    movie card component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { FetchAllMovies } from "../services/apis";
 
 const Home = () => {
+  const [allMovies, setAllMovies] = useState([]);
   const getAllMovies = async () => {
     try {
       const res = await FetchAllMovies();
@@ -15,7 +17,29 @@ const Home = () => {
     getAllMovies();
   }, []);
 
-  return <>Home</>;
+  return (
+    <Container>
+      <main>
+        <Row lg={4} className="main">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map(() => (
+            <Col>
+              <Card className="movie_card">
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                  <Card.Title>Card Title</Card.Title>
+                  <Card.Text>
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </main>
+    </Container>
+  );
 };
 
 export default Home;
