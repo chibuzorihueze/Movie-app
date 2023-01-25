28d3628abedcfba700aeee5acb575bfe535ef77b
unknown <krisykord@gmail.com>
2023-01-25 09:50:37 -0800

    movie card component and UI fixes

--- src/components/common/Card/index.js
+++ src/components/common/Card/index.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 
 const MovieCard = ({ id, image, title }) => (
   <Col>
-    <Card className="movie_card">
-      <Card.Img variant="top" src={image} width={227} height={350} />
+    <Card className="movie_card bg-dark text-white">
+      <Card.Img variant="top" src={image} width={227} height={300} />
       <Card.Body>
         <Link to={`/${id}`} className="movie_title text-uppercase">
           {title}
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,1 +1,2 @@
 export { default as TopNav } from "./Nav";
+export { default as MoviesCard } from "./Card";
