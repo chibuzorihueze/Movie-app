6745a3c3e079e7aa75d4974dd9f247124d8f7864
unknown <krisykord@gmail.com>
2023-01-25 08:46:34 -0800

    fetchALlMovies and card coomponent

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { FetchAllMovies } from "../services/apis";
 
-const Home = () => <>Home</>;
+const Home = () => {
+  const getAllMovies = async () => {
+    try {
+      const res = await FetchAllMovies();
+      console.log(res);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getAllMovies();
+  }, []);
+
+  return <>Home</>;
+};
 
 export default Home;
--- src/services/axios/index.js
+++ src/services/axios/index.js
@@ -1,4 +1,4 @@
-const { default: axios } = require("axios");
+import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://imdb-api.com/en/API/",
