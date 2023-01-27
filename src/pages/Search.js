9f060197954c94f21d9fa9ce44ee44c57dc04274
unknown <krisykord@gmail.com>
2023-01-26 23:51:13 -0800

    search page and component

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
-const Search = () => (
-  <div className="container mx-auto text-white py-5 ">
-    <div className="w-50 d-flex justify-content-between">
-      <Form.Control
-        size="md"
-        type="text"
-        placeholder="Search Movies"
-        className="me-5 bg-transparent text-white"
-      />
-      <button className="btn btn-success">Search</button>
+const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (e) => {
+    const { value } = e.target;
+    setSearchValue(value);
+  };
+
+  return (
+    <div className="container mx-auto text-white py-5 ">
+      <div className="w-50 d-flex justify-content-between">
+        <Form.Control
+          size="md"
+          type="text"
+          placeholder="Search Movies"
+          className="me-5 bg-transparent text-white"
+          onChange={handleSearch}
+          value={searchValue}
+        />
+        <button className="btn btn-success">Search</button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Search;
