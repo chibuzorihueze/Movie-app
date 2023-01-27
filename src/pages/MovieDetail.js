ea79faa033e4fb134cf8ac2cf83406d0bbadca20
unknown <krisykord@gmail.com>
2023-01-26 16:43:22 -0800

    setting movieDetailcomponents

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { fetchMovieByIdApi } from "../services/apis";
 
@@ -10,6 +11,7 @@ const MovieDetail = () => {
     try {
       const res = await fetchMovieByIdApi(id);
       console.log(res);
+      setMovieDetail(res);
     } catch (err) {
       throw err;
     }
@@ -19,7 +21,28 @@ const MovieDetail = () => {
     fetchMovieDetail();
   }, [fetchMovieDetail]);
 
-  return <div>MovieDetail</div>;
+  return (
+    <div className="text-white">
+      <Container className="py-5 mx-auto">
+        <Row>
+          <Col className="border" lg={4}>
+            <div>
+              <img
+                src={movieDetail.image}
+                alt="movie_cover"
+                width={350}
+                height={500}
+              />
+            </div>
+          </Col>
+          <Col lg={6}>
+            <h1>{movieDetail.title}</h1>
+            <p>{movieDetail.plot}</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default MovieDetail;
