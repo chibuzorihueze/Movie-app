f448b454b073b1f029b5c9caa94ffa1aa5793404
unknown <krisykord@gmail.com>
2023-01-27 00:38:51 -0800

    Movies genre card and lists

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -26,7 +26,61 @@ const genres = [
 ];
 
 const Movies = () => {
-  const [currGenre, setCurrGenre] = useState("Action");
+  const [currGenre, setCurrGenre] = useState("");
+  const [allMovies, setAllMovies] = useState([
+    {
+      id: "tt1740828",
+      image:
+        "https://m.media-amazon.com/images/M/MV5BNGM4NTRlZjAtNzM1Ni00YTQ5LTk3YWItNWJhNmM3ZTZlYWExXkEyXkFqcGdeQXVyMTEyNzgwMDUw._V1_Ratio0.6837_AL_.jpg",
+      title: "Wonder Woman",
+      description: "(2011 TV Movie)",
+      runtimeStr: "45 min",
+      genres: "Action, Adventure, Fantasy",
+      genreList: [
+        {
+          key: "Action",
+          value: "Action",
+        },
+        {
+          key: "Adventure",
+          value: "Adventure",
+        },
+        {
+          key: "Fantasy",
+          value: "Fantasy",
+        },
+      ],
+      contentRating: null,
+      imDbRating: "4.2",
+      imDbRatingVotes: "2232",
+      metacriticRating: null,
+      plot: "It's the modern day, and being Wonder Woman is complicated. Diana is leading a triple life - running a large corporation out of costume and fighting crime in costume in one identity.",
+      stars:
+        "Jeffrey Reiner, Pedro Pascal, Adrianne Palicki, Cary Elwes, Elizabeth Hurley",
+      starList: [
+        {
+          id: "tt1740828",
+          name: "Jeffrey Reiner",
+        },
+        {
+          id: "tt1740828",
+          name: "Pedro Pascal",
+        },
+        {
+          id: "tt1740828",
+          name: "Adrianne Palicki",
+        },
+        {
+          id: "tt1740828",
+          name: "Cary Elwes",
+        },
+        {
+          id: "tt1740828",
+          name: "Elizabeth Hurley",
+        },
+      ],
+    },
+  ]);
 
   const getMoviesByGenre = useCallback(async () => {
     try {
@@ -45,7 +99,7 @@ const Movies = () => {
     <Container className="mx-auto w-75 my-4">
       <div
         className="genres d-flex flex-wrap"
-        style={{ gap: "15px 15px", opacity: 1, transform: "none" }}
+        style={{ gap: "15px", opacity: 1, transform: "none" }}
       >
         {genres.map(({ id, name }) => (
           <Button
@@ -59,7 +113,9 @@ const Movies = () => {
         ))}
       </div>
       <div className="my-5">
-        <MovieCard />
+        {allMovies.map(({ id, image, title }) => (
+          <MovieCard key={id} id={id} image={image} title={title} />
+        ))}
       </div>
     </Container>
   );
